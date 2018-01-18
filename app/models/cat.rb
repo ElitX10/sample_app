5adde393ef45fc5d2c071724615d76af776eb924
ElitX10 <thomas.lescolan@sfr.fr>
2018-01-18 11:59:42 +0100

    modify cats model

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -1,2 +1,21 @@
 class Cat < ApplicationRecord
+  validates :name, presence: {}, uniqueness: {}
+  validates :email, presence: {}, uniqueness: {}
+  validates :password, presence: {}
+
+  before_save :encrypt_password
+
+  def has_password?(password_soumis)
+    password == encrypt(password_soumis)
+  end
+
+  private
+
+  def encrypt_password
+    self.password = encrypt(password)
+  end
+
+  def encrypt(string)
+    string
+  end
 end
